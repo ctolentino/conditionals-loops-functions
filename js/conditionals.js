1cd0a3d3f3a12013622a20021494f4bf9e2658a5
Chenelle Tolentino <ctolentino@myhta.org>
2017-07-14 14:09:10 -1000

    practice 7-14

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,9 +1,21 @@
 /*WHO ARE YOU?
 * Create a variable called "name" assign it a value of your name
 * Write a conditional statement that prints out "Hi" + name
-  if truthy, otherwise prints out "goodbye"
+  if truthy (it is true), otherwise prints out "goodbye"
 */
 
+var name = "tolentino";
+
+function nameTest(){
+
+if(name === "tolentino"){
+    return "Hi " + name;
+  }else{
+    return "goodbye";
+  }
+}
+
+console.log(nameTest());
 
 /* HIGHER VALUE
 * Create a variable called "num1", assign it a value of 10
@@ -12,6 +24,18 @@
   prints out the higher value.
 */
 
+var num1 = 10;
+var num2 = 5;
+
+function compareNum(){
+if(num1>num2){
+    return num1;
+  } else{
+    return num2;
+  }  
+}
+
+console.log(compareNum(num1, num2));
 
 /* AGE CHECKER [Look up logical/comparitive operators]
 * Create a variable named "age", assign it your age
@@ -19,8 +43,26 @@
   * age >= 16 < 18 = print "You are old enough to drive"
   * age >= 18 < 21 = print "You are old enough to vote"
   * age >= 21 = print "You are old enough to rent a car"
-  * other = print "Sorry your to young"
+  * other = print "Sorry you're to young"
 */
+
+var age = 20;
+
+function ageChecker(){
+  if(age >= 16 < 18){
+    return "You are old enough to drive";
+  } 
+//use else ifs when you want to exit out of an attached if statement
+  else if(age >= 18 < 21 ){
+    return "You are old enough to vote";
+  } else if(age >= 21 ){
+    return "You are old enough to rent a car";
+  } else{
+    return "Sorry you're to young";
+  }  
+}
+
+console.log(ageChecker(age));
 
 
 /* GRADE CHECKER [Look up logical/comparitive operators]
@@ -34,3 +76,21 @@
   * other = print "Sorry we don't have a grade for that score "
 */
 
+var testScore = 59;
+
+function gradeChecker(){
+  if(testScore >= 50 && testScore<=59){
+    return "You got an F, try harder";
+  }
+  else if(testScore >= 50 && testScore<=59){
+    return "You got a D, not good enough";
+  }
+  else if(testScore >= 50 && testScore<=59){
+    return "You got a C, I think you can do better";
+  }
+  else if(testScore >= 50 && testScore<=59){
+
+  }
+}
+
+console.log(gradeChecker(testScore));
